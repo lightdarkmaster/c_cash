cc421b5175335c20b7a377c0821236efc9a30f18
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-06-14 00:23:41 +0800

    Update code

--- lib/pages/FAQ.dart
+++ lib/pages/FAQ.dart
@@ -31,7 +31,7 @@ class _FAQState extends State<FAQ> {
               const QuestionWithAnswer(
                 question: 'How can send money to others?',
                 answer:
-                    'Go to send Money and enter the desire amount you want to send, and then go to send transactions',
+                    'Go to send Money and enter the desire amount you want to send, and then go to send transactions and see history',
               ),
               const SizedBox(height: 16),
               const QuestionWithAnswer(
